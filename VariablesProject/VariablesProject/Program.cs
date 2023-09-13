e722226fdb1e77d757d33a1af87e6e8ab2432060
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-09-13 13:51:29 -0300

    Added score HUD

--- VariablesProject/VariablesProject/Program.cs
+++ VariablesProject/VariablesProject/Program.cs
@@ -14,11 +14,21 @@ namespace VariablesProject
             Console.WriteLine("--------------");
             Console.WriteLine();
 
+            // whole number = integer (0,1,2,3,-1,-2)
 
-            // score
+
+            // declaring score variable to be integer type
             int score;
+            
+            // initializing score
 
+            score = 0;
 
+            // hud
+            Console.WriteLine(score);
+
+            
+            
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         
