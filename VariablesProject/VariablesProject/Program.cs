ce3825b0ea2c57b99df4f0abad3cd1e79b672bad
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-09-13 14:21:52 -0300

    Added name, values, format

--- VariablesProject/VariablesProject/Program.cs
+++ VariablesProject/VariablesProject/Program.cs
@@ -19,19 +19,40 @@ namespace VariablesProject
 
             // declaring score variable to be integer type
             int score;
-            
-            // initializing score
+            int enemyValue;
+            string realName;
+            string gamerTag;
+            float percentile;
 
+            
+            
+            // initializing 
+            enemyValue = 250;
             score = 0;
+            realName = "Ryan LeBlanc";
+            gamerTag = "Ryphil";
+            percentile = 85.543f;
+
+
+            // pretend gameplay
+            score = score + enemyValue; // "=" means assign or get value
+            // score += enemyValue; "+=" same thing as ^
 
             // hud
-            Console.WriteLine(score);
+            Console.WriteLine(realName + " (" + gamerTag + ")");
+            
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Percentile Rank: " + percentile);
+            
+            Console.WriteLine();
+            Console.WriteLine("Hi, " + realName + ". " + "How are you?");
 
-            
-            
+
+
+            Console.WriteLine();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
-        
+            
         
         }
     }
