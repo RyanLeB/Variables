784462841831336ac20c938cd0472c477f2122b2
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-09-13 13:38:25 -0300

    Variables project created

---
+++ VariablesProject/VariablesProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables.");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        
+        
+        }
+    }
+}
